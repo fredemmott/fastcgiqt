e78d788cc3dbc6933591aad93419cc9db8e7f6da
Fred Emmott <mail@fredemmott.co.uk>
2009-01-28 00:34:31 +0000

    Start on Doxygen-based documentation

--- include/FastCgiQt/Request.h
+++ include/FastCgiQt/Request.h
@@ -6,30 +6,199 @@
 
 namespace FastCgiQt
 {
+	/// Class containing information about a request.
 	class Request
 	{
 		public:
+			/// Construct an invalid Request object.
 			Request();
+			/** Construct a valid Request object, for the specified
+			 * request id.
+			 *
+			 * @param requestId is a FastCGI request ID.
+			 */
 			Request(quint16 requestId);
+
+			/** If this Request object is actually valid.
+			 *
+			 * An invalid Request object is not actually associated
+			 * with a client request.
+			 */
 			bool isValid() const;
+			/// The FastCGI request ID.
 			quint16 requestId() const;
 
+			/** The type of the POST-data (if any).
+			 *
+			 * @see contentLength()
+			 * @see content()
+			 * @see haveAllContent()
+			 * @see waitForAllContent()
+			 * @see postData(const QString&) const
+			 * @see postData() const
+			 */
 			QString contentType() const;
+
+			/** The length of the POST data.
+			 *
+			 * @see contentType()
+			 * @see content()
+			 * @see haveAllContent()
+			 * @see waitForAllContent()
+			 * @see postData(const QString&) const
+			 * @see postData() const
+			 */
 			quint64 contentLength() const;
+
+			/** The POST data sent in this request.
+			 *
+			 * If it has not all been received, this method will
+			 * block until the upload has been finished
+			 *
+			 *
+			 * @see Responder::in for a stream.
+			 * @see contentType()
+			 * @see contentLength()
+			 * @see haveAllContent()
+			 * @see waitForAllContent()
+			 * @see postData(const QString&) const
+			 * @see postData() const
+			 */
 			QByteArray content() const;
+
+			/** Whether all the POST data has been uploaded by the
+			 * client.
+			 *
+			 * @see contentType()
+			 * @see contentLength()
+			 * @see content()
+			 * @see waitForAllContent()
+			 * @see postData(const QString&) const
+			 * @see postData() const
+			 */
 			bool haveAllContent() const;
+
+			/** Block until all POST data has been uploaded by the
+			 * client.
+			 *
+			 * @see contentType()
+			 * @see contentLength()
+			 * @see content()
+			 * @see haveAllContent()
+			 * @see postData(const QString&) const
+			 * @see postData() const
+			 */
 			void waitForAllContent() const;
 
+			/** Retrieve a POST form value.
+			 *
+			 * If the content-type is not
+			 * application/x-www-urlencoded, or the specified form
+			 * value wasn't sent, this will return a null QString.
+			 *
+			 * If the content-type is application/x-www-urlencoded,
+			 * but there is still POST data which hasn't been
+			 * received yet, this member will block until it has all
+			 * been received.
+			 *
+			 * This is roughly equivalent to $_POST[$name] in PHP.
+			 *
+			 * @param name is the name of the value to try and
+			 * 	retrieve.
+			 *
+			 * @see contentType()
+			 * @see contentLength()
+			 * @see content()
+			 * @see haveAllContent()
+			 * @see waitForAllContent()
+			 * @see postData() const
+			 */
 			QString postData(const QString& name) const;
+
+			/** Retrieve all POST form data.
+			 *
+			 * This will return an empty QHash if the content-type
+			 * is not application/x-www-urlencoded, or if there
+			 * were no properly-encoded name-value pairs.
+			 *
+			 * If the content-type is application/x-www-urlencoded,
+			 * but there is still POST data which hasn't been
+			 * received yet, this member will block until it has all
+			 * been received.
+			 *
+			 * This is roughly equivalent to $_POST in PHP.
+			 *
+			 * @see contentType()
+			 * @see contentLength()
+			 * @see content()
+			 * @see haveAllContent()
+			 * @see waitForAllContent()
+			 * @see postData(const QString&) const
+			 */
 			QHash<QString, QString> postData() const;
 
+			/** Retrieve a specific server variable.
+			 *
+			 * If the specified server variable is not set, this
+			 * will return a null QString.
+			 *
+			 * This is roughly equivalent to $_SERVER[$name] in PHP.
+			 *
+			 * @param name is the name of the server variable to be
+			 * 	retrieved.
+			 *
+			 * @see serverData() const
+			 */
 			QString serverData(const QString& name) const;
+
+			/** Retrieve all server variables.
+			 *
+			 * These variables are equivalent to the environment
+			 * variables used by CGI, such as the HTTP headers,
+			 * QUERY_STRING, and so on.
+			 *
+			 * This is roughly equivalent to $_SERVER in PHP.
+			 *
+			 * @see serverData(const QString&) const
+			 */
 			QHash<QString, QString> serverData() const;
 
+			/** Retrieve a GET form value.
+			 *
+			 * This returns a value which is form-encoded in the
+			 * query string.
+			 *
+			 * This is roughly equivalent to $_GET[$name] in PHP.
+			 *
+			 * @param name is the name of the value to retrieve.
+			 *
+			 * @see getData() const
+			 */
 			QString getData(const QString& name) const;
+
+			/** Retrieve all GET form values.
+			 *
+			 * This returns all name-value form data pairs from the
+			 * query string.
+			 *
+			 * This is roughly equivalent to $_GET in PHP.
+			 */
 			QHash<QString, QString> getData() const;
 
+			/** @internal
+			 * Add some newly-received server variables to this
+			 * Request object.
+			 *
+			 * This should only be called by a FastCgiQt::Manager
+			 * object.
+			 */
 			void addServerData(const QHash<QString, QString>& data);
+			/** @internal
+			 * Add some newly-received post data.
+			 *
+			 * This should only be called by a FastCgiQt::Manager
+			 * object.
+			 */
 			void appendContent(const QByteArray& data);
 		private:
 			bool m_isValid;
--- include/FastCgiQt/Responder.h
+++ include/FastCgiQt/Responder.h
@@ -9,26 +9,95 @@ class QIODevice;
 
 namespace FastCgiQt
 {
+	/** Abstract class for responding to HTTP requests.
+	 *
+	 * All web applications will involve at least one subclass of this
+	 * class.
+	 */
 	class Responder : public QObject
 	{
 		Q_OBJECT
 		public:
+			/// Respond to a web request.
 			virtual void respond() = 0;
+			/** Clean up the responder.
+			 *
+			 * Among other things, this will flush the output stream.
+			 */
 			virtual ~Responder();
 		protected:
+			/** Construct a responder.
+			 *
+			 * You shouldn't actually have to do anything with these
+			 * parameters except pass them through to this
+			 * constructor; especially, do not use the socket
+			 * directly.
+			 *
+			 * @param request contains information on the request.
+			 * @param socket is the socket being used for FastCGI in
+			 * 	this request.
+			 * @param inputDevice is a streaming input device.
+			 * @param parent is a QObject* parent.
+			 */
 			Responder(const Request& request, QIODevice* socket, QIODevice* inputDevice, QObject* parent = NULL);
 
+			/// Class containing information about the request.
 			const Request& request;
+			/** Input text stream (POST data).
+			 *
+			 * This is the HTTP post data, as a text stream.
+			 * If you need the raw device, use in.device().
+			 *
+			 * The Request class has other (possible more
+			 * convenient) ways to access this data, although
+			 * not as a stream.
+			 *
+			 * @see Request::postData() const
+			 * @see Request::postData(const QString&) const
+			 * @see Request::content()
+			 * @see Request::contentType()
+			 * @see Request::contentLength()
+			 * @see Request::haveAllContent()
+			 * @see Request::waitForAllContent()
+			 */
 			QTextStream in;
+
+			/** Output text stream.
+			 *
+			 * Text written to this stream will be sent to the
+			 * client.
+			 *
+			 * If you need the raw device, use out.device().
+			 */
 			QTextStream out;
 	};
+
+	/** A typedef for a pointer to a factory function for Responder objects.
+	 *
+	 * This function must have the signature:
+	 * @code
+	 * void myResponderGenerator(const Request&, QIODevice*, QIODevice*, QObject*);
+	 * @endcode
+	 *
+	 * @relates Responder
+	 */
 	typedef Responder* (*ResponderGenerator)(const Request&, QIODevice*, QIODevice* inputDevice, QObject*);
 }
 
-#define RESPONDER(x) \
+/** Convenience macro for creating constructor and factories of Responder
+ * subclasses.
+ *
+ * The generated constructor just calls the Responder constructor.
+ *
+ * The generated factory function is called 'instance', and a pointer to it is
+ * a FastCgiQt::ResponderGenerator.
+ *
+ * @relates FastCgiQt::Responder
+ */
+#define RESPONDER(className) \
 	public: \
-		x(const FastCgiQt::Request& request, QIODevice* socket, QIODevice* inputDevice, QObject* parent = NULL) \
+		className(const FastCgiQt::Request& request, QIODevice* socket, QIODevice* inputDevice, QObject* parent = NULL) \
 			: Responder(request, socket, inputDevice, parent) {} \
 		static Responder* instance(const FastCgiQt::Request& request, QIODevice* socket, QIODevice* inputDevice, QObject* parent) \
-			{ return new x(request, socket, inputDevice, parent); }
+			{ return new className(request, socket, inputDevice, parent); }
 #endif
