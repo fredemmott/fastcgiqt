77c5a75f5adef081373dcf988f4cf99e723480d1
Fred Emmott <mail@fredemmott.co.uk>
2009-01-28 13:54:28 +0000

    document Manager class

--- include/FastCgiQt/Manager.h
+++ include/FastCgiQt/Manager.h
@@ -8,13 +8,27 @@
 namespace FastCgiQt
 {
 	class ManagerPrivate;
+	/** Class managing FastCGI connections.
+	 *
+	 * This class is responsible for listening to the FastCGI socket,
+	 * and acting on whatever data is received.
+	 * 
+	 * This includes spawning Responder classes.
+	 */
 	class Manager : public QObject
 	{
 		Q_OBJECT
 		public:
+			/** Create a Manager.
+			 *
+			 * @param responderGenerator is a pointer to a function creating Responder objects.
+			 * @param parent is the parent object.
+			 */
 			Manager(ResponderGenerator responderGenerator, QObject* parent = NULL);
+			/// Destroy the Manager.
 			~Manager();
 		private:
+			///@internal
 			ManagerPrivate* d;
 	};
 }
