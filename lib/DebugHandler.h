dc8979db68d747d22f4b4de926080d9d77a3d8d2
Fred Emmott <mail@fredemmott.co.uk>
2009-01-29 21:24:29 +0000

    document DebugHandler

--- lib/DebugHandler.h
+++ lib/DebugHandler.h
@@ -7,15 +7,35 @@
 
 namespace FastCgiQt
 {
+	/** @internal @brief Class for redirection qDebug, qFatal, and so on.
+	 *
+	 * This redirects to syslog, as required by the FastCGI spec.
+	 */
 	class DebugHandler : public QObject
 	{
 		public:
+			/** Start redirecting messages.
+			 *
+			 * Also opens syslog for this process.
+			 */
 			DebugHandler(QObject* parent = NULL);
+			/** Stop redirecting messages.
+			 *
+			 * Also closes sylog for this process.
+			 */
 			~DebugHandler();
 		private:
+			/// Send the specified message to syslog.
 			static void syslogHandler(QtMsgType type, const char* message);
 
+			/// The old Qt message handler.
 			QtMsgHandler m_oldHandler;
+			/** The name of the application.
+			 *
+			 * This is taken from QCoreApplication::applicationName().utf8().
+			 *
+			 * A copy is made, as syslog wants a const char* to it.
+			 */
 			const QByteArray m_name;
 	};
 };
