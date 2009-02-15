d5ecfb69a67cf780a0881fbae42da1dc96f0ac93
Fred Emmott <mail@fredemmott.co.uk>
2009-02-15 17:14:58 +0000

    add RequestCacheMaintainer which expires Request Cache entries if a file used in responding to the request has been modified

---
+++ lib/RequestCacheMaintainer.cpp
@@ -0,0 +1,64 @@
+#include "RequestCacheMaintainer.h"
+
+#include <QDebug>
+#include <QFileSystemWatcher>
+#include <QMutexLocker>
+#include <QWriteLocker>
+
+namespace FastCgiQt
+{
+	QMutex RequestCacheMaintainer::m_instanceMutex;
+	QHash<Cache*, QPointer<RequestCacheMaintainer> > RequestCacheMaintainer::m_instances;
+
+	RequestCacheMaintainer* RequestCacheMaintainer::instance(Cache* cache)
+	{
+		QMutexLocker lock(&m_instanceMutex);
+		if(!m_instances.contains(cache))
+		{
+			m_instances.insert(cache, new RequestCacheMaintainer(cache));
+		}
+		return m_instances.value(cache);
+	}
+
+	RequestCacheMaintainer::RequestCacheMaintainer(Cache* cache, QObject* parent)
+		:
+			QObject(parent),
+			m_cache(cache),
+			m_watcher(new QFileSystemWatcher(this))
+
+	{
+		connect(
+			m_watcher,
+			SIGNAL(fileChanged(QString)),
+			this,
+			SLOT(removeAssociatedEntries(QString))
+		);
+	}
+
+	void RequestCacheMaintainer::addDependency(const QString& urlFragment, const QString& file)
+	{
+		QMutexLocker lock(&m_watcherMutex);
+		if(!m_filesToFragments.contains(file))
+		{
+			m_watcher->addPath(file);
+		}
+		if(!m_filesToFragments.contains(file, urlFragment))
+		{
+			m_filesToFragments.insert(file, urlFragment);
+		}
+	}
+
+
+	void RequestCacheMaintainer::removeAssociatedEntries(const QString& path)
+	{
+		QMutexLocker lock(&m_watcherMutex);
+		m_watcher->removePath(path);
+
+		QWriteLocker writeLock(m_cache->readWriteLock()); // so we don't have to re-acquire the lock for every removal
+		Q_FOREACH(const QString& urlFragment, m_filesToFragments.values(path))
+		{
+			m_cache->remove(urlFragment);
+		}
+		m_filesToFragments.remove(path);
+	}
+}
