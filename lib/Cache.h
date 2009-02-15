6afdf02859fb2f14608ee11fa8af4656f07d6cdb
Fred Emmott <mail@fredemmott.co.uk>
2009-02-15 15:48:30 +0000

    document Cache structure

--- lib/Cache.h
+++ lib/Cache.h
@@ -10,17 +10,31 @@ class QReadWriteLock;
 
 namespace FastCgiQt
 {
+	/// A cache entry composed of data and a timestamp.
 	struct CacheEntry
 	{
 		CacheEntry(const QDateTime& timeStamp, const QByteArray& data);
 		const QDateTime timeStamp;
 		const QByteArray data;
 	};
+	/** A read-write-locked cache structure.
+	 *
+	 * This is a QCache<QString, CacheEntry> with a recursive QReadWriteLock.
+	 *
+	 * All members lock as neccessary, but you may need to lock at an outer
+	 * scope depending on the usage.
+	 *
+	 * Keep in mind that even with a recursive lock, the locks must be of
+	 * the same type; for example, all the writing calls will deadlock
+	 * if called while a read lock is open.
+	 */
 	class Cache : private QCache<QString, CacheEntry>
 	{
 		public:
+			/
 			Cache(int maxSize);
 			~Cache();
+			/// Return a pointer to the QReadWriteLock.
 			QReadWriteLock* readWriteLock() const;
 
 			void clear();
