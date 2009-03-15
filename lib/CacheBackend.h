dc86fef44f6f43ccc294956d9895dff9af4749dc
Fred Emmott <mail@fredemmott.co.uk>
2009-03-15 16:38:47 +0000

    Added CacheBackend documentation

--- lib/CacheBackend.h
+++ lib/CacheBackend.h
@@ -9,19 +9,42 @@ class QReadWriteLock;
 
 namespace FastCgiQt
 {
+	/** Abstract interface for cache backends.
+	 *
+	 * Implementations are expected to use a QReadWriteLock as neccessary.
+	 * If the implementation doesn't require it, a valid pointer should
+	 * still be returned for readWriteLock in case it's required for
+	 * higher-level classes (such as FileDependentCache).
+	 *
+	 * @see CacheBackend::Factory
+	 *
+	 * @ingroup plugins
+	 */
 	class CacheBackend
 	{
 		public:
+			/// Remove an entry from the cache.
 			virtual void remove(const QString& key) = 0;
+			/// Fetch a value from the cache.
 			virtual CacheEntry value(const QString& key) const = 0;
+			/// Write a value to the cache.
 			virtual void setValue(const QString& key, const CacheEntry& entry) = 0;
 
+			/// Retrieve the read-write lock.
 			virtual QReadWriteLock* readWriteLock() const = 0;
 
+			/** Factory class constructing a CacheBackend.
+			 *
+			 * This is a qt plugin interface.
+			 *
+			 * @ingroup plugins
+			 */
 			class Factory
 			{
 				public:
+					/// Load any settings relevant for this cache backend.
 					virtual void loadSettings();
+					/// Create a CacheBackend*
 					virtual CacheBackend* getCacheBackend(const QString& cacheName) const = 0;
 			};
 	};
