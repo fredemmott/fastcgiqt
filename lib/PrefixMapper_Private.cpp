64fb2d221a76e4f9c64fb0dcb8ca2a5b35573717
Fred Emmott <mail@fredemmott.co.uk>
2009-10-01 18:36:56 +0100

    add PrefixMapper_Private missing file

---
+++ lib/PrefixMapper_Private.cpp
@@ -0,0 +1,30 @@
+#include "PrefixMapper_Private.h"
+#include "Request.h"
+
+#include <QVariant>
+
+const char* PrefixTrackingProperty = "FASTCGIQT_PREFIXMAPPER_PREFIX";
+const char* SuffixTrackingProperty = "FASTCGIQT_PREFIXMAPPER_SUFFIX";
+
+namespace FastCgiQt
+{
+	void PrefixMapper::Private::setPrefix(Request* request, const QString& value)
+	{
+		request->setProperty(PrefixTrackingProperty, value);
+	}
+
+	void PrefixMapper::Private::setSuffix(Request* request, const QString& value)
+	{
+		request->setProperty(SuffixTrackingProperty, value);
+	}
+
+	QString PrefixMapper::Private::prefix(Request* request)
+	{
+		return request->property(PrefixTrackingProperty).toString();
+	}
+
+	QString PrefixMapper::Private::suffix(Request* request)
+	{
+		return request->property(SuffixTrackingProperty).toString();
+	}
+};
