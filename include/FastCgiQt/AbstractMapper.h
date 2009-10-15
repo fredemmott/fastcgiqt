0c687b67c0c56099c9f0c437bc71bdda85e163e2
Fred Emmott <mail@fredemmott.co.uk>
2009-10-15 08:21:21 +0100

    add headers (no implementation yet) for AbstractMapper and PostDataStreamReader

---
+++ include/FastCgiQt/AbstractMapper.h
@@ -0,0 +1,61 @@
+/* LICENSE NOTICE
+	Copyright (c) 2009, Frederick Emmott <mail@fredemmott.co.uk>
+
+	Permission to use, copy, modify, and/or distribute this software for any
+	purpose with or without fee is hereby granted, provided that the above
+	copyright notice and this permission notice appear in all copies.
+
+	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+	WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+	MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+	ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+	WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+	ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+#pragma once
+
+#include "AbstractResponder.h"
+
+namespace FastCgiQt
+{
+	/** Map requests to services.
+	 *
+	 * This class is for mapping URLs to QObjects and slots.
+	 *
+	 * @see AbstractMapper
+	 * @ingroup services
+	 */
+	class AbstractMapper: public QObject
+	{
+		Q_OBJECT;
+		public:
+			enum ResponseMode
+			{
+				BlockingResponses,
+				ThreadedResponses
+			};
+
+			AbstractMapper(ResponseMode mode = BlockingResponses, QObject* parent = 0);
+			virtual ~AbstractMapper();
+
+			/// Add a spawner to the mapping.
+			void addMapping(const QString& serviceName, SpawnerBase* spawner, const char* slot);
+			/** Add a slot to the mapping.
+			 *
+			 * The receiver *MUST NOT* have a parent, and *MUST* be threadsafe.
+			 */
+			void addMapping(const QString& serviceName, QObject* receiver, const char* slot);
+		public slots:
+			void respond(FastCgiQt::Request*);
+		protected:
+			/** Return which of the specified service names match.
+			 *
+			 * @p services is a QBytearray containing the service names, in percent-encoded UTF-8.
+			 */
+			virtual int indexIn(const QList<QByteArray>& services) const = 0;
+		private:
+			class Private;
+			Private* d;
+	};
+}
---
+++ include/FastCgiQt/PostDataStreamReader.h
@@ -0,0 +1,60 @@
+#pragma once
+
+class QByteArray;
+class QIODevice;
+namespace FastCgiQt
+{
+	class PostDataStreamReader
+	{
+		public:
+			enum TokenType
+			{
+				Invalid       = 0,
+				NoToken       = 1,
+				StartData     = 2,
+				VariableName  = 3,
+				VariableValue = 4,
+				FileHeader    = 5,
+				FileValue     = 6,
+				EndData       = 7
+			};
+			PostDataStreamReader(FastCgiQt::Request* request);
+			~PostDataStreamReader();
+
+			bool atEnd() const;
+			TokenType tokenType() const;
+			TokenType readNext() const;
+
+			/** The name of the variable.
+			 *
+			 * Valid for VariableName, VariableValue, FileHeader, and FileValue
+			 */
+			QString variableName() const;
+
+			/** Contents of the current variable/file.
+			 *
+			 * This is a streaming device.
+			 *
+			 * Valid for VariableValue and FileValue.
+			 */
+			QIODevice* content() const;
+
+			/** Name of the file being uploaded.
+			 *
+			 * This is provided by the user-agent, so should be sanitised.
+			 *
+			 * Valid for FileHeader.
+			 */
+			QString filename() const;
+			/** MIME type of the file being uploaded.
+			 *
+			 * This is provided by the user-agent.
+			 *
+			 * Valid for FileHeader.
+			 */
+			QString mimeType() const;
+		private:
+			class Private;
+			Private* d;
+	};
+};
